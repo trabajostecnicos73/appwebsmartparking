10998200a41d513bbc1bede211a3cf17842d478b
Eduar Triviño Clavijo <trabajostecnicos73@gmail.com>
2026-02-16 21:15:48 -0500

    tercer commit

--- consumidor/vite.config.js
+++ consumidor/vite.config.js
@@ -3,9 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5180,
-    strictPort: true,
-    host: true, // <--- ESTO ES VITAL para que el celular se conecte
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
   }
 })
